8c52baf772f97a58f0a72464755c2c71e391473c
Joe Malott <joe@onebadgoose.com>
2024-01-14 19:28:16 -0600

    Changes

--- .eleventy.js
+++ .eleventy.js
@@ -3,40 +3,65 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 
+// Import fast-glob package
+const fg = require('fast-glob');
+
+
+
+
+
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
-
+  
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
-
+  
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLL yyyy"
-    );
-  });
-
-  // Syntax Highlighting for Code blocks
-  eleventyConfig.addPlugin(syntaxHighlight);
-
-  // To Support .yaml Extension in _data
-  // You may remove this if you can use JSON
-  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
-
-  // Copy Static Files to /_Site
-  eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
+      );
+    });
+    
+    
+    
+    // Syntax Highlighting for Code blocks
+    eleventyConfig.addPlugin(syntaxHighlight);
+    
+    // To Support .yaml Extension in _data
+    // You may remove this if you can use JSON
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+    
+    // Copy Static Files to /_Site
+    eleventyConfig.addPassthroughCopy({
+      "./src/admin/config.yml": "./admin/config.yml",
+      "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
+      "./node_modules/prismjs/themes/prism-tomorrow.css":
       "./static/css/prism-tomorrow.css",
+    });
+    
+    // Copy Image Folder to /_site
+    eleventyConfig.addPassthroughCopy("./src/static/img");
+    
+    // Copy favicon to route of /_site
+    eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+    
+
+    // Run search for images in /static/img
+const mainImages = fg.sync(['**/img/**/*.jpg', '!**/_site']);
+
+
+// Create collection of gallery images
+eleventyConfig.addCollection('images', function (collection) {
+  return mainImages.map(imagePath => {
+    // Assuming your site is hosted at the root and _site is the output directory
+    return '/static/img/' + imagePath.split('/').pop();
   });
+});
 
-  // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("./src/static/img");
 
-  // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+
 
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
@@ -53,6 +78,9 @@ module.exports = function (eleventyConfig) {
     return content;
   });
 
+
+
+
   // Let Eleventy transform HTML files as nunjucks
   // So that we can use .html instead of .njk
   return {
@@ -61,4 +89,7 @@ module.exports = function (eleventyConfig) {
     },
     htmlTemplateEngine: "njk",
   };
+
+
+
 };
